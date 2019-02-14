86c45dd141cbbaa7ae7d42f7e275686030d36ff2
Jon-Fehr <41446236+Jon-Fehr@users.noreply.github.com>
2019-02-14 14:48:58 -0600

    Added Gui
    Added in all the basic needs for this project on the GUI.

--- Price-Calc/Price-Calc/Form1.Designer.cs
+++ Price-Calc/Price-Calc/Form1.Designer.cs
@@ -28,13 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnOpen1 = new System.Windows.Forms.Button();
+            this.cboOpen = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.cboOpen2 = new System.Windows.Forms.ComboBox();
+            this.btnOpen2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(468, 11);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(84, 13);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Price Calculator ";
+            // 
+            // btnOpen1
+            // 
+            this.btnOpen1.Location = new System.Drawing.Point(29, 42);
+            this.btnOpen1.Name = "btnOpen1";
+            this.btnOpen1.Size = new System.Drawing.Size(128, 32);
+            this.btnOpen1.TabIndex = 1;
+            this.btnOpen1.Text = "Supplier Inv Sheet";
+            this.btnOpen1.UseVisualStyleBackColor = true;
+            // 
+            // cboOpen
+            // 
+            this.cboOpen.FormattingEnabled = true;
+            this.cboOpen.Location = new System.Drawing.Point(163, 53);
+            this.cboOpen.Name = "cboOpen";
+            this.cboOpen.Size = new System.Drawing.Size(188, 21);
+            this.cboOpen.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(29, 104);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(484, 410);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(546, 104);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(500, 409);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // cboOpen2
+            // 
+            this.cboOpen2.FormattingEnabled = true;
+            this.cboOpen2.Location = new System.Drawing.Point(680, 53);
+            this.cboOpen2.Name = "cboOpen2";
+            this.cboOpen2.Size = new System.Drawing.Size(188, 21);
+            this.cboOpen2.TabIndex = 6;
+            // 
+            // btnOpen2
+            // 
+            this.btnOpen2.Location = new System.Drawing.Point(546, 42);
+            this.btnOpen2.Name = "btnOpen2";
+            this.btnOpen2.Size = new System.Drawing.Size(128, 32);
+            this.btnOpen2.TabIndex = 7;
+            this.btnOpen2.Text = "Yakesh Inv Sheet";
+            this.btnOpen2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1079, 533);
+            this.Controls.Add(this.btnOpen2);
+            this.Controls.Add(this.cboOpen2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cboOpen);
+            this.Controls.Add(this.btnOpen1);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnOpen1;
+        private System.Windows.Forms.ComboBox cboOpen;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.ComboBox cboOpen2;
+        private System.Windows.Forms.Button btnOpen2;
     }
 }
 
